fdbc152d1c91641fc4522555ef1aa9978f74de99
Jun Xii <izwjx21@gmail.com>
2026-03-07 17:15:03 +0800

    driverDashboard

--- src/roles/driver/dashboard.js
+++ src/roles/driver/dashboard.js
@@ -1,6 +1,27 @@
 /* Page script: dashboard (driver) */
 function initDriverDashboard() {
-  
+    const offerRideBtn = document.querySelector('.offerRideButton button');
+    const modal = document.getElementById('offerRideModal');
+    const close = modal.querySelector('.close');
+
+    modal.style.display = 'none';
+    
+    //open
+    offerRideBtn.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    });
+
+    // close 
+    close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    });
+
+    // close when press outside
+    window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+    });
 }
 
 initDriverDashboard();
